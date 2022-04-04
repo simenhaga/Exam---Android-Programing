adfbe98df6971c1ddfe6b8b0fb3b72992c6337ed
simenhaga <simen.haga@lyse.net>
2022-04-04 12:35:02 +0200

    Added 3 buttons and positioned them

--- app/src/main/java/no/kristiania/MainActivity.kt
+++ app/src/main/java/no/kristiania/MainActivity.kt
@@ -2,10 +2,16 @@ package no.kristiania
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonThree = findViewById<Button>(R.id.button3)
+        val buttonFour = findViewById<Button>(R.id.button4)
+        val buttonFive = findViewById<Button>(R.id.button5)
+
     }
 }
