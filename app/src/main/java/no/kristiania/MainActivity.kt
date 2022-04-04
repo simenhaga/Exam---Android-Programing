eecdf7ba0218e9cae64014282c5e7fe53a35734f
simenhaga <simen.haga@lyse.net>
2022-04-04 14:22:23 +0200

    Added two extra buttons and tried to witch fragment onClick

--- app/src/main/java/no/kristiania/MainActivity.kt
+++ app/src/main/java/no/kristiania/MainActivity.kt
@@ -1,10 +1,16 @@
 package no.kristiania
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,6 +18,17 @@ class MainActivity : AppCompatActivity() {
         val buttonThree = findViewById<Button>(R.id.button3)
         val buttonFour = findViewById<Button>(R.id.button4)
         val buttonFive = findViewById<Button>(R.id.button5)
+        switchFragment()
 
+
+    }
+
+
+    private fun switchFragment() {
+        val buttonThree = findViewById<Button>(R.id.button3)
+        val fragment = SelectImageFragment()
+        buttonThree.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit()
+        }
     }
 }
