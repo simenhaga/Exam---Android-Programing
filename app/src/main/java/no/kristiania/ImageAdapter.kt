759c6071559e6fb28bd500fe151e619f86ec4c7b
simenhaga <simen.haga@lyse.net>
2022-04-22 16:32:39 +0200

    Now you can save results to the database. LETSGOOOO TGIF

--- app/src/main/java/no/kristiania/ImageAdapter.kt
+++ app/src/main/java/no/kristiania/ImageAdapter.kt
@@ -5,18 +5,23 @@ import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.image_rv_layout.view.*
 
-class ImageAdapter(val context: Context?, private val imageList: List<ImageApi>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+class ImageAdapter(val context: Context?, private val imageList: List<ImageApi>): RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
+    class ViewHolder(val view: View): RecyclerView.ViewHolder(view){
+        val saveButton: Button = view.findViewById(R.id.saveImageBtn)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.image_rv_layout, parent, false)
 
         return ViewHolder(v)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         imageList[position].imageUri?.let {
             holder.itemView.apiImage.setImageURI(Uri.parse(it))
         }
@@ -25,13 +30,22 @@ class ImageAdapter(val context: Context?, private val imageList: List<ImageApi>)
                 .load(imageList[position].imageUrl)
                 .into(holder.itemView.apiImage)
         }
+
+        holder.saveButton.setOnClickListener {
+            imageList[position].imageUrl?.let{ url ->
+                context?.let { context ->
+                    Globals.getDatabase(context).saveImagesFromResult(StoredResultsModel(imageLink = url))
+                }
+            }
+        }
+
     }
 
     override fun getItemCount(): Int {
         return imageList.size
     }
 
-    class ViewHolder(v:View?) : RecyclerView.ViewHolder(v!!), View.OnClickListener{
+    /*class ViewHolder(val view:View?) : RecyclerView.ViewHolder(view!!), View.OnClickListener{
         override fun onClick(p0: View?) {
             //onClick function here
         }
@@ -41,5 +55,5 @@ class ImageAdapter(val context: Context?, private val imageList: List<ImageApi>)
         }
 
         val apiImage = itemView.apiImage!!
-    }
+    }*/
 }
