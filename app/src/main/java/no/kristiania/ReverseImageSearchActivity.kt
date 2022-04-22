f40e5b7382199b137c1b7998fd79176acb23b5be
MathiasLandro <71821491+mathiaslandro@users.noreply.github.com>
2022-04-22 15:47:53 +0200

    startet on saving images from result

--- app/src/main/java/no/kristiania/ReverseImageSearchActivity.kt
+++ app/src/main/java/no/kristiania/ReverseImageSearchActivity.kt
@@ -3,22 +3,30 @@ package no.kristiania
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import no.kristiania.databinding.ActivityReverseImageSearchBinding
+import no.kristiania.databinding.ImageRvLayoutBinding
+import java.io.File
 
 class ReverseImageSearchActivity : AppCompatActivity() {
     lateinit var binding: ActivityReverseImageSearchBinding
+    lateinit var saveBtnBinding: ImageRvLayoutBinding
+    private var imageResult: File? = null
 
     private lateinit var imageRV: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityReverseImageSearchBinding.inflate(layoutInflater)
+        saveBtnBinding = ImageRvLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
         title = "ReverseImageSearchApp"
+        val database = Globals.getDatabase(baseContext)
 
         // Init views
         imageRV = binding.apiRecyclerView
@@ -31,6 +39,14 @@ class ReverseImageSearchActivity : AppCompatActivity() {
                 apiController.reversedImageSearchBing(it) { imageList ->
                     imageRV.adapter = ImageAdapter(this@ReverseImageSearchActivity, imageList)
                 }
+            }
+        }
+
+        saveBtnBinding.saveImageBtn.setOnClickListener{
+            imageResult?.let {
+                database.saveImagesFromResult(StoredResultsModel(imageLink = imageResult?.toUri().toString()))
+                Log.d("Database", "${database.getImages()}")
+                apiController.uploadFileToServer(it) { result -> Globals.uploadUrl = result }
             }
         }
 
